d99247e65dff948cb6b072a632a15ab5a6be188c
Frank Lauren <lawrain21@gmail.com>
2019-07-22 12:21:55 +0700

    modus feedback

--- ex4.js
+++ ex4.js
@@ -1,33 +1,35 @@
-function cariModus(arr) {
-    // you can only write your code here!
-    var angkaModus=[];
-    for (var i = 0; i<arr.length; i++) {
-        var modus=0;
-        for (var j = 0; j<arr.length; j++) {
-            if (arr[i]===arr[j] && i !== j) {   //revisi, tadinya harus modus == 2, karena ketemu 2x angkanya      
-                modus++;
-                if (modus === arr.length-1) {
-                    return -1;
-                }
-                else {            
-                    angkaModus.push(arr[j]);
-                }
+function cariModus(num) {
+    var sameNum;
+    var mostSame = 1;
+    for (var i=0; i<num.length; i++) {
+        var sameCount = 0;
+        for (var k=0; k<num.length; k++) {
+            if (num[i]===num[k]) {
+                sameCount++;
             }
+            // console.log(mostSame + "<" + sameCount)
+            if (mostSame<sameCount) {
+                mostSame = sameCount;
+                sameNum = num[k];
+                // console.log("k = " + k + " num = " + sameNum);
+            }    
         }
-        // console.log("modus " + modus);
     }
-    // console.log("modus = " + modus);
-    if (angkaModus == false) {           //klo tidak ada angka modus
+    if (sameCount === num.length) {
         return -1;
     }
-    else {
-        return angkaModus[0];
+    if (sameNum === undefined) {
+        return -1;
     }
+    return sameNum;
 }
-  
-  // TEST CASES
-  console.log(cariModus([10, 4, 5, 2, 4])); // 4
-  console.log(cariModus([5, 10, 10, 6, 5])); // 5
-  console.log(cariModus([10, 3, 1, 2, 5])); // -1
-  console.log(cariModus([1, 2, 3, 3, 4, 5])); // 3
-  console.log(cariModus([7, 7, 7, 7, 7])); // -1
+ 
+ 
+ 
+ // TEST CASES
+ console.log(cariModus([10, 4, 5, 2, 4])); // 4
+ console.log(cariModus([5, 10, 10, 6, 5])); // 5
+ console.log(cariModus([10, 3, 1, 2, 5])); // -1
+ console.log(cariModus([1, 2, 3, 3, 4, 5])); // 3
+ console.log(cariModus([7, 7, 7, 7, 7])); // -1
+ console.log(cariModus([10, 4, 5, 5, 5, 2, 4]));
